ffcb922260134352910b5cfbdcde1447aa484487
kirsten tay <taykirsten11@gmail.com>
2021-08-06 03:09:52 +0000

    added the Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+
+const hero=() => {
+    return (
+     
+          <div className=" flex flex-row flex justify-between items-center bg-red-600 px-10 border-b border-gray-200">
+            
+          </div>
+        </div>
+   
+    
+    )
+
+}
+export default Hero;
+
+
