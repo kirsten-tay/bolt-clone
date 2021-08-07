459319a95f5f959c833ae6f8e2bfbb6ef5b66ab2
kirsten tay <taykirsten11@gmail.com>
2021-08-07 22:16:26 +0000

    modified the Navbar and Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,25 @@
 import react from 'react';
+import image from './../Assets/image.jpg';
 
 const Hero=() => {
     return (
      
-          <div className=" flex flex-row justify-between items-center ">
-            <div className="flex-col bg-green-600">
-            <h1 class="text-3xl">The fast, affordable way to ride.</h1>
-            <p class="text-center download-app-text text-md-left">Download the Bolt app</p>
-            <p class="text-center request-bolt-online-text text-md-left">Request Bolt online without an app</p>
+          <div className=" flex flex-row  ">
+            <div className=" flex flex-col justify-center w-screen md:w-1/2 relativw w-full items-center bg-green-600">
+          <div className="m-5 text-white"> 
+            <h1 class=" text-4xl ">The fast, affordable way to ride.</h1>
+            < span className="  download-app-text text-md-left">Download the Bolt app</span>
+           </div>
             <div className="app-badges">
                 <a href="https://go.onelink.me/app/9c5a3c35/" className="app-store-button">
                     <img src="https://bolt.eu/static/8de0c47bd0804c7387b44eb24f7af748/en.svg" class="w-13 h-10"alt="app-badges.app-store-alt"/></a>
                 <a href="https://go.onelink.me/app/d81f73c6/" class="play-store-button">
                     <img  src="https://bolt.eu/static/fc711801f3c70175cf946b523b97ac30/en.svg"class="w-15 h-10" alt="app-badges.play-store-alt"/></a>
                     </div>
+            </div>
+            <div className="flex flex-col justify-center  w-screen md:w-1/2 relative w-full">
+                <img src={image}/>
+
             </div>
           </div>
 
