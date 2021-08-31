6826e836d52e2598e22e72ceb9e5552de6b9a991
kirsten tay <taykirsten11@gmail.com>
2021-08-31 21:36:57 +0000

    updated the phone section

--- src/components/phoneSec.jsx
+++ src/components/phoneSec.jsx
@@ -4,8 +4,14 @@ import react from 'react'
 const phoneSec=() => {
     return(
 <div className=" flex-row   bg-lightgreen-600">
+    <div className="flex flex-col">
                 <h1>Get a ride in minutes!</h1>
                 <span>Pick your destination, request a ride, meet your driver, enjoy the journey.</span>
+            </div>
+<div className="flex flex-col">
+    
+</div>
+
             </div>
 
 
