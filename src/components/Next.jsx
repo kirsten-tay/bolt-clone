3f11311eeca9f52d1a21aa6178393ed8cb914e41
kirsten tay <taykirsten11@gmail.com>
2021-09-02 21:33:48 +0000

    updated the Next section

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -3,7 +3,32 @@ import react from'react';
 
 const Next=() =>{
     return(
-
+ 
+        <div className="flex row">
+          <div className="flex-col ">
+            
+    <h1> Safe and convenient</h1> 
+      <span>
+      Moving with Bolt is easy, convenient and fast.
+      </span>
+          </div>
+          <div className="flex-col ">
+            
+    <h1> Happy drivers, happy riders</h1> 
+      <span>
+      Bolt drivers earn more thanks to lower commission rates.
+      </span>
+          </div>
+          
+          <div className="flex-col ">
+            
+    <h1> Always there for you</h1> 
+      <span>
+      Get fast support, whenever you need it.
+      </span>
+          </div>
+          
+        </div>
 
 
 
