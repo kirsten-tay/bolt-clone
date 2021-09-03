7eca30e53b38ea0e4c3ae5dc6979540be22cccc3
kirsten tay <taykirsten11@gmail.com>
2021-09-03 20:55:27 +0000

    updated the Next section

---
+++ src/components/CallSvg.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function CallSvg() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+      <path
+        stroke="#2EB875"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10.8 18.2c1.2-.3 2.4-.8 3.4-1.3.9.5 1.8.9 2.8 1.3.5.2.9.7.7 1.3l-.3.9c-.1.5-.8.8-1.4.6-6-2.2-10.8-7-12.9-13.1-.2-.6 0-1.3.6-1.4l.9-.3c.5-.1 1.1.2 1.3.7.3 1 .8 1.9 1.3 2.8-.5 1.1-1 2.2-1.3 3.4"
+      ></path>
+      <path
+        stroke="#2EB875"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M16 3c-2.8 0-5 2.2-5 5v5h5c2.8 0 5-2.2 5-5s-2.2-5-5-5z"
+      ></path>
+    </svg>
+  );
+}
+
+export default CallSvg;
--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -1,27 +1,28 @@
 import react from'react';
-
+import carSvg from './carSvg'
+import HeartSvg from './HeartSvg'
 
 const Next=() =>{
     return(
  
         <div className="flex row">
           <div className="flex-col ">
-            
+            <carSvg/>
     <h1> Safe and convenient</h1> 
       <span>
       Moving with Bolt is easy, convenient and fast.
       </span>
           </div>
           <div className="flex-col ">
-            
+            <HeartSvg/>
     <h1> Happy drivers, happy riders</h1> 
       <span>
       Bolt drivers earn more thanks to lower commission rates.
       </span>
           </div>
-          
+
           <div className="flex-col ">
-            
+            <CallSvg/>
     <h1> Always there for you</h1> 
       <span>
       Get fast support, whenever you need it.
