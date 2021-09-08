6cd7bef183c6441b831eea45473574435278ed4d
kirsten tay <taykirsten11@gmail.com>
2021-09-08 23:04:27 +0000

    updated the project

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -9,23 +9,23 @@ const Next=() =>{
         <div className="flex flex-row my-10">
           <div className="flex-col justify-center  w-screen md:w-1/2 relative w-full">
             <carSvg/>
-    <h1> Safe and convenient</h1> 
-      <span>
+    <span className="text-md">Safe and convenient</span><br/> 
+      <span className="text-xs">
       Moving with Bolt is easy, convenient and fast.
       </span>
           </div>
           <div className="flex-col justify-center  w-screen md:w-1/2 relative w-full ">
             <HeartSvg/>
-    <h1> Happy drivers, happy riders</h1> 
-      <span>
+    <span className="text-md"> Happy drivers, happy riders</span><br/> 
+      <span className="text-xs">
       Bolt drivers earn more thanks to lower commission rates.
       </span>
           </div>
 
           <div className="flex-col justify-center  w-screen md:w-1/2 relative w-full">
             <CallSvg/>
-    <h1> Always there for you</h1> 
-      <span>
+    <span className="text-md"> Always there for you</span><br/> 
+      <span className="text-xs">
       Get fast support, whenever you need it.
       </span>
           </div>
--- src/components/phoneSec.jsx
+++ src/components/phoneSec.jsx
@@ -6,13 +6,13 @@ const PhoneSec=() => {
     return (
          <div className="flex flex-row bg-green-600 my-10">
              <div className="flex-col justify-center  w-screen md:w-1/2 relative w-full">
-                 <div>
+                 <div className="my-10">
                  <h3>Get a ride in minutes!</h3>
              <p class="mt-4 text-xs">Pick your destination, request a ride, meet your driver, enjoy the journey.</p>
              </div>
              </div>
-             <div className="flex-col justify-center  w-screen md:w-1/2 relative w-full">
-            <div className="">
+             <div className="flex-col justify-center  relative w-full">
+            <div>
             <img src={'https://bolt.eu/img/index-phone.png'}  />
             </div>
          </div>
