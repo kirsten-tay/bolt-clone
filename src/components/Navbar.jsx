310c120fd6f0f989fc987287aee6210fbfec8cb7
Kirsten <newuser@KirstenMacBookAir.local>
2021-10-12 23:40:11 +0000

    fixed bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,21 @@ import LogoSvg from './../Assets/LogoSvg'
 const Navbar=() => {
     return (
      
-          <div className=" flex flex-row  justify-between items-center bg-white-600 px-5  w-screen md:w-1/2 relative w-full">
-            <div className="  flex-1">
+          <div className=" flex flex-row  justify-between item-center bg-white-600 px-5 py-5 w-screen md:w-1/2 relative w-full">
+            <div className="  flex-1 mx-10">
               <LogoSvg/>
 
          </div>
             <div className=" flex-1  ">
-            <nav className="space-x-5 flex mx-3">
-              <a className="text-xs">Ride</a>
-              <a className="text-xs">Become a rider</a>
-              <a className="text-xs">Fleet</a>
-              <a className="text-xs">Business</a>
-           <a className="text-xs">Scooters</a>
-             <a className="text-xs">Drive</a>
-              <a className="text-xs">Food</a>
-               <a className="text-xs">Cities</a>
+            <nav className="space-x-10 flex mx-10">
+              <a className="text-s">Ride </a>
+              <a className="text-s">Become a rider</a>
+              <a className="text-s">Fleet</a>
+              <a className="text-s">Business</a>
+           <a className="text-s">Scooters</a>
+             <a className="text-s">Drive</a>
+              <a className="text-s">Food</a>
+               <a className="text-s">Cities</a>
 
 
 
