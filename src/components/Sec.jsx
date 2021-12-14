0a4a708a075bd565d16106962716b1e916fa1615
Kirsten tay <taykirsten11@gmail.com>
2021-12-14 18:18:09 +0000

    updated the sec

--- src/components/Sec.jsx
+++ src/components/Sec.jsx
@@ -4,8 +4,8 @@ import React from'react'
 
 const Sec=() => {
     return (
-         <div className="flex flex-row bg-blue-600 ">
-             <div class=" text-md-left my-10 mx-5">
+         <div className="flex flex-row  bg-blue-900 ">
+             <div class=" text-md-left my-10 mx-5  ">
                  <h3>Earn extra money driving</h3>
              <p class=" text-xs">Set your own schedule, be your own boss.</p></div>
             <div className="bg-circle bg-circle-left hide show-sm">
